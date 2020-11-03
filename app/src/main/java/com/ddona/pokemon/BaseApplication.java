029b7b6468d181e7a4461a3587e613d7aa22fd1b
Trung Doan <doanptand@gmail.com>
2020-11-03 20:05:20 +0700

    init structer and add dependencies

---
+++ app/src/main/java/com/ddona/pokemon/BaseApplication.java
@@ -0,0 +1,6 @@
+package com.ddona.pokemon;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+}
--- app/src/main/java/com/ddona/pokemon/ui/MainActivity.java
+++ app/src/main/java/com/ddona/pokemon/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ddona.pokemon;
+package com.ddona.pokemon.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ddona.pokemon.R;
 
 public class MainActivity extends AppCompatActivity {
 
